cf2301f7bf92318e7274a8ca880fddcd668e01de
mathsant-js <matheus.santana18@fatec.sp.gov.br>
2026-03-27 21:01:54 -0300

    feat: entendendo o conceito de interfaces funcionais

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,23 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         MusicPlayer musicPlayer = new Computer();
         VideoPlayer videoPlayer = new Computer();
+
+        List<User> users = List.of
+                (
+                    new User("Matheus", 18),
+                    new User("Henrique", 18),
+                    new User("Tino", 18)
+                );
+
+        users.forEach(user -> {
+            System.out.println(user);
+            System.out.println(user.getClass().getCanonicalName());
+        });
 
         runMusic(musicPlayer);
         runVideo(videoPlayer);
@@ -16,4 +30,6 @@ public class Main {
     public static void runVideo(VideoPlayer videoPlayer) {
         videoPlayer.playVideo();
     }
+
+
 }
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public record User(String name, int age) {
+}
